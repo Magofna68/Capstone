d8cd3817e7b0b07a13bfec6f2b1b43f8564da4ee
Brandon-Magofna <magofna68@gmail.com>
2021-07-23 10:53:59 -0700

    correct img formatting to properly reflect on page

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,8 +4,8 @@ import CardInfo from '../components/CardInfo';
 function Card(props) {
 
   return (
-    <div className="g-card" onClick={(e) => props.click(props.item)}>
-      <img className="g-card-image" src={props.item.imgSrc} alt={props.item.imgSrc} />
+    <div className="" style={{ display: "inline-block", width: "27%", margin: "10px 0px" }} onClick={(e) => props.click(props.item)}>
+      <img className="test" src={props.item.imgSrc} alt={props.item.imgSrc} />
       {props.item.selected && <CardInfo title={props.item.title} subTitle={props.item.subTitle} />}
     </div>
 
