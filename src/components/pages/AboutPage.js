d9e79b9b9d13ec97765b6b85ea9f27c2818e954d
Brandon-Magofna <magofna68@gmail.com>
2021-07-23 13:46:26 -0700

    finished :hammer: out About page; placeholder text used

--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -8,7 +8,24 @@ function AboutPage(props) {
       <Hero title={props.title} />
 
       <Content>
-        Hello
+        <h2><bold>Lorem ipsum:</bold></h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est placerat in egestas erat. Neque egestas congue quisque egestas. Eget nunc lobortis mattis aliquam faucibus purus in. Viverra accumsan in nisl nisi scelerisque. Maecenas pharetra convallis posuere morbi. Id donec ultrices tincidunt arcu non sodales neque sodales. Faucibus a pellentesque sit amet porttitor eget dolor. Nisi scelerisque eu ultrices vitae auctor eu augue. Vestibulum morbi blandit cursus risus at ultrices mi tempus. Velit egestas dui id ornare arcu. Arcu risus quis varius quam quisque id diam. Mi tempus imperdiet nulla malesuada pellentesque. Vitae auctor eu augue ut lectus arcu bibendum. Vivamus arcu felis bibendum ut tristique et. Ut ornare lectus sit amet est placerat in egestas. Dui nunc mattis enim ut tellus elementum sagittis vitae et.
+        </p>
+        <p>
+          Nibh mauris cursus mattis molestie a iaculis at erat pellentesque. Nunc lobortis mattis aliquam faucibus purus in. Dignissim cras tincidunt lobortis feugiat vivamus at augue eget arcu. Eget sit amet tellus cras adipiscing. Pulvinar pellentesque habitant morbi tristique. Ante metus dictum at tempor commodo. Scelerisque fermentum dui faucibus in ornare. Amet porttitor eget dolor morbi non arcu. Dictum sit amet justo donec. Dolor sit amet consectetur adipiscing elit ut. Facilisis sed odio morbi quis commodo odio aenean sed. Dignissim suspendisse in est ante in nibh mauris cursus mattis. Blandit aliquam etiam erat velit scelerisque in dictum.
+        </p>
+        <h3><bold>Lorem ipsum:</bold></h3>
+        <p>
+          Sit amet tellus cras adipiscing enim. Erat velit scelerisque in dictum non consectetur. Malesuada fames ac turpis egestas maecenas pharetra convallis posuere. Rhoncus mattis rhoncus urna neque viverra. Semper viverra nam libero justo. Tellus in metus vulputate eu scelerisque felis imperdiet. Aliquam eleifend mi in nulla posuere sollicitudin. In nibh mauris cursus mattis molestie a iaculis at. Tristique senectus et netus et malesuada fames ac. Venenatis urna cursus eget nunc. Iaculis eu non diam phasellus vestibulum lorem sed risus. Aliquet eget sit amet tellus cras. Aliquet eget sit amet tellus cras adipiscing enim eu turpis. Sit amet porttitor eget dolor morbi non. Cursus euismod quis viverra nibh cras pulvinar. Diam ut venenatis tellus in metus vulputate eu. Et malesuada fames ac turpis egestas. Felis eget nunc lobortis mattis aliquam. Laoreet suspendisse interdum consectetur libero id faucibus. Vivamus arcu felis bibendum ut tristique et egestas quis ipsum.
+        </p>
+        <h4>Lorem ipsum:</h4>
+        <p>
+          Aenean sed adipiscing diam donec adipiscing. Nisl nisi scelerisque eu ultrices vitae auctor eu augue. Elementum curabitur vitae nunc sed velit dignissim sodales ut. Pellentesque pulvinar pellentesque habitant morbi. Urna cursus eget nunc scelerisque viverra mauris in. Mi proin sed libero enim sed faucibus turpis. Dignissim sodales ut eu sem integer. A arcu cursus vitae congue mauris rhoncus aenean. Tellus in metus vulputate eu. Netus et malesuada fames ac turpis. Eleifend mi in nulla posuere sollicitudin aliquam. Consequat mauris nunc congue nisi vitae suscipit tellus mauris. Tempus imperdiet nulla malesuada pellentesque elit eget gravida cum. Elementum curabitur vitae nunc sed velit dignissim sodales. Donec ac odio tempor orci dapibus ultrices. Scelerisque mauris pellentesque pulvinar pellentesque. Dignissim enim sit amet venenatis urna. Ac ut consequat semper viverra nam libero justo. Nunc lobortis mattis aliquam faucibus purus in massa tempor. Duis ultricies lacus sed turpis tincidunt id.
+        </p>
+        <p>
+          Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit. Scelerisque fermentum dui faucibus in. Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Interdum varius sit amet mattis vulputate enim. Volutpat odio facilisis mauris sit amet massa vitae. Quam quisque id diam vel. Massa vitae tortor condimentum lacinia. Pulvinar elementum integer enim neque volutpat ac. Purus in mollis nunc sed id semper risus in. Molestie at elementum eu facilisis sed odio morbi quis commodo. Risus in hendrerit gravida rutrum quisque non tellus orci. Felis bibendum ut tristique et. Viverra maecenas accumsan lacus vel facilisis volutpat est velit egestas. In hendrerit gravida rutrum quisque. Venenatis cras sed felis eget velit aliquet sagittis id. Bibendum est ultricies integer quis auctor. Diam vel quam elementum pulvinar etiam non quam lacus suspendisse. Cursus in hac habitasse platea dictumst. In hac habitasse platea dictumst quisque sagittis purus sit.
+        </p>
       </Content>
 
     </div>
