63594f9f7e8640c9fe3fb27edbb64695cc4d48e7
Brandon-Magofna <magofna68@gmail.com>
2021-07-23 10:26:36 -0700

    display pictures w/ corresponding card info

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,12 +4,12 @@ import CardInfo from '../components/CardInfo';
 function Card(props) {
 
   return (
-    <div className="inline-block g-card">
+    <div className="g-card" onClick={(e) => props.click(props.item)}>
       <img className="g-card-image" src={props.item.imgSrc} alt={props.item.imgSrc} />
       {props.item.selected && <CardInfo title={props.item.title} subTitle={props.item.subTitle} />}
     </div>
 
   );
 }
-// onClick={(e) => props.click(props.item)}
+
 export default Card;
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -38,7 +38,8 @@ class Carousel extends React.Component {
       ]
     }
   }
-  handleCardClick = (id, card) => {
+  handleCardClick = (id) => {
+    console.log(id)
     let items = [...this.state.items];
 
     items[id].selected = items[id].selected ? false : true;
@@ -55,7 +56,7 @@ class Carousel extends React.Component {
 
   makeItems = (items) => {
     return items.map(item => {
-      return <Card item={item} onClick={(e => this.handleCardClick(item.id, e))} key={item.id} />
+      return <Card item={item} click={(e => this.handleCardClick(item.id, e))} key={item.id} />
     })
   }
 
